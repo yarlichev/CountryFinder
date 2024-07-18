7bfbf2a30daa79e5f08ec1aa15d346a8fd64d5fe
Ilia.Iarlychev.26007360 <Ilia.Iarlychev@microfocus.com>
2024-07-18 02:51:07 +0200

    Update CountryCodeDao.java

--- src/main/java/com/countryfinder/repositories/CountryCodeDao.java
+++ src/main/java/com/countryfinder/repositories/CountryCodeDao.java
@@ -1,6 +1,7 @@
 package com.countryfinder.repositories;
 
 import com.countryfinder.models.CountryCode;
+import com.countryfinder.models.CountryCodeId;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CountryCodeDao extends CrudRepository<CountryCode, String> {
+public interface CountryCodeDao extends CrudRepository<CountryCode, CountryCodeId> {
 
 
     // first search for all codes which are first digits of the number
